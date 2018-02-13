eee18f8befdca47b4ded594624ef25f54ed778a7
enzio53 <codemaxgames@gmail.com>
2018-02-13 20:19:23 +0100

    Fixed Version

--- SMethods/functions/SEncrypt.class.php
+++ SMethods/functions/SEncrypt.class.php
@@ -9,12 +9,6 @@ class SEncrypt{
 
     private $alphabet = "123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ";
 
-    /**
-     * @param $str
-     * @param $secret_key
-     * @param $secret_iv
-     * @return string
-     */
     public function sha256_encode($str, $secret_key, $secret_iv){
         $key    = hash('sha256', $secret_key);
         $iv     = substr(hash('sha256', $secret_iv), 0, 16);
@@ -22,12 +16,6 @@ class SEncrypt{
         return base64_encode(openssl_encrypt($str, "AES-256-CBC", $key, 0, $iv));
     }
 
-    /**
-     * @param $str
-     * @param $secret_key
-     * @param $secret_iv
-     * @return string
-     */
     public function sha256_decode($str, $secret_key, $secret_iv){
         $key    = hash('sha256', $secret_key);
         $iv     = substr(hash('sha256', $secret_iv), 0, 16);
@@ -35,28 +23,14 @@ class SEncrypt{
         return openssl_decrypt(base64_decode($str), "AES-256-CBC", $key, 0, $iv);
     }
 
-    /**
-     * @param $str
-     * @return string
-     */
     public function bin_decode($str){
         return pack('H*', $str);
     }
 
-    /**
-     * @param $str
-     * @return mixed
-     */
     public function bin_encode($str){
         return array_shift(unpack('H*', $str));
     }
 
-    /**
-     * @param $int
-     * @return string
-     *
-     * Encode base58 (work with only int)
-     */
     public function base58_encode($int) {
         $base58_string  = "";
         $base           = strlen($this->alphabet);
@@ -71,12 +45,6 @@ class SEncrypt{
         return $base58_string;
     }
 
-    /**
-     * @param $base58
-     * @return float|int
-     *
-     * Decode base58
-     */
     public function base58_decode($base58) {
         $int_val = 0;
         for($i=strlen($base58)-1,$j=1,$base=strlen($this->alphabet);$i>=0;$i--,$j*=$base) {
